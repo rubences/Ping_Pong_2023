adeb93245921d7200028c52f0f3b235cedc67af3
rubences <91731294+rubences@users.noreply.github.com>
2023-12-14 17:16:02 +0000

    sleep

--- Game.java
+++ Game.java
@@ -23,6 +23,7 @@ public class Game {
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
         executor.execute(player1);
+        
         executor.execute(player2);
 
         sleep(2);
