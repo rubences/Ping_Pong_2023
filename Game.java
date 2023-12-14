3814c8c5fa43fdf9aeeea2ff6044fe2e891af325
rubences <91731294+rubences@users.noreply.github.com>
2023-12-14 16:01:40 +0000

    modificar la clase game para los loks

--- Game.java
+++ Game.java
@@ -2,15 +2,17 @@ public class Game {
 
     public static void main(String[] args) {
 
-        Player player1 = new Player("ping");
-        Player player2 = new Player("pong");
+        Object lock = new Object();
+
+        Player player1 = new Player("ping", lock);
+        Player player2 = new Player("pong", lock);
 
         player1.setNextPlayer(player2);
         player2.setNextPlayer(player1);
 
         System.out.println("Game starting...!");
 
-        player1.setMustPlay(true);
+        player1.setPlay(true);
 
         Thread thread2 = new Thread(player2);
         thread2.start();
